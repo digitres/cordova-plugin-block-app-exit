9c1f0fb8dfbae34b6d441bf8895efde6ac4dd35a
Lenny Chavhunduka <lenny@digitres.com>
2022-12-22 19:05:26 +0200

    blockign feature

--- src/android/com/digitres/cordova/plugin/block-app-exit.kt
+++ src/android/com/digitres/cordova/plugin/block-app-exit.kt
@@ -15,19 +15,49 @@ import androidx.core.content.ContextCompat
 
 
 class BlockAppExit: CordovaPlugin(){
-
+    private exitAllowed: bool = true
+    private context: CallbackContext
     override fun execute(
         action: String,
         args: JSONArray,
         callbackContext: CallbackContext
     ): Boolean {
             // blockExit()
-            echo("Message from Plugin",callbackContext)
+            context = callbackContext
+            if (action == 'disable') {
+                blockEnabled = true
+                echo("App exit disabled",callbackContext)
+            } else if (action == 'enable') {
+                blockEnabled = false
+                echo("App exit enabled",callbackContext)
+            }
+
             return true
         }
 
-    // this just brings the current activity back to the front hence preventing user from leaving
-//    private fun blockExit() {
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            Toast.makeText(BlockAppExit, "Has focus", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(BlockAppExit, "Focus changed", Toast.LENGTH_SHORT).show()
+        }
+
+//        if (!hasFocus) {
+//            val windowCloseExecutor = ContextCompat.getMainExecutor(this)
+//            windowCloseExecutor.execute {
+//                val am = applicationContext.getSystemService(ACTIVITY_SERVICE) as ActivityManager
+//                val cn = am.getRunningTasks(1)[0].topActivity
+//                if (cn != null && cn.className == "com.android.systemui.recent.RecentsActivity") {
+//                    blockRecentButton()
+//                }
+//            }
+//        }
+    }
+
+
+//    override fun onPause() {
+//        super.onPause()
 //        val activityManager = applicationContext
 //            .getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
 //        activityManager.moveTaskToFront(taskId, 0)
@@ -35,9 +65,10 @@ class BlockAppExit: CordovaPlugin(){
 
 
     private fun echo(
-        message: String= "Test echo message",
+        message: String,
         callbackContext: CallbackContext
     ) {
+        Toast.makeText(context, "Echo from Plugin", Toast.LENGTH_SHORT).show();
         if (message.isNotEmpty()) {
             callbackContext.success(message);
         } else {
